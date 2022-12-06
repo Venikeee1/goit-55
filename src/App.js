74a7d18e61063c27ffcc1ad0e02bd93b1787d8f4
oleksandr_vienik <alexandrVenik@gmail.com>
2022-12-06 18:55:24 +0200

    qa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import { Routes, Route } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useState, useRef } from 'react';
 import MainLayout from './Layouts/MainLayout';
 import './App.css';
+import './qa/interview-tasks';
+import { ErrorBoundary } from './qa/ErrorBoundary';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 const Homepage = lazy(() => import('./pages/Home/Home'));
 const PostPage = lazy(() => import('./pages/Post/Post'));
 
+const CustomForm = ({ onSubmit }) => {
+  const [formData, setFormData] = useState({
+    query: '',
+  });
+  const [clicks, setClicks] = useState(0);
+  const formRef = useRef();
+  /** const usersClickRef = useRef(0);
+  // console.log(`clicked ${usersClickRef.current} times; in rerenders`);
+  console.log(`clicked ${clicks} times; in rerenders`);
+  */
+
+  const handleClick = () => {
+    // usersClickRef.current++;
+    // setClicks((prevState) => prevState + 1);
+    // console.log(`clicked ${usersClickRef.current} times`);
+  };
+
+  const handleChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+
+    setFormData({ [name]: value });
+  };
+
+  const handleSubmit = () => {
+    onSubmit(formData);
+  };
+  useEffect(() => {
+    console.log(formRef.current, '--formRef');
+  }, []);
+
+  // throw new Error('super error');
+
+  return (
+    <form ref={formRef} onSubmit={handleSubmit} onClick={handleClick}>
+      <input type="text" name="query" onChange={handleChange} />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+const FormPortal = () =>
+  createPortal(<CustomForm />, document.getElementById('modals'));
+
 const App = () => {
+  const handleSubmit = (formData) => {
+    fetch('/someAddress', formData);
+  };
+
   return (
     <Suspense fallback={<>Page is loading...</>}>
+      <ErrorBoundary fallbackComponent={<>There was an error </>}>
+        <CustomForm onSubmit={handleSubmit} />
+      </ErrorBoundary>
+      <FormPortal />
+
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route
